571c5975a77fb87d3a4eac6f950611b557b216b7
Daniel Lima <daniel.lima.braga@gmail.com>
2022-06-28 09:59:29 -0500

    versão 1.0

---
+++ exemplo02/exemplo2.pas
@@ -0,0 +1,39 @@
+unit exemplo2;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    btCalcular: TButton;
+    bbSair: TButton;
+    procedure FormCreate(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+
+end;
+
+end.
+
